604349d8f726889542e64f2ae62bf8fa22a6acba
jakubmanczak <jakub@manczak.net>
2024-08-20 00:44:00 +0200

    show users as cards with admin badge

--- facade/src/app/users/page.tsx
+++ facade/src/app/users/page.tsx
@@ -1,9 +1,80 @@
+"use client";
 import { Dashboard } from "@/components/Dashboard";
+import { Card } from "@/components/ui/card";
+import { qfetch } from "@/lib/qfetch";
+import { user } from "@/types/user";
+import { LucideShieldCheck, LucideUser } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function UsersPage() {
+  const [users, setUsers] = useState<user[]>([]);
+  const [fetchStat, setFetchStat] = useState<{
+    status: number;
+    statusText: string;
+  } | null>(null);
+  const getUsers = async () => {
+    const res = await qfetch("/users");
+    setFetchStat({
+      status: res.status,
+      statusText: res.statusText,
+    });
+    if (!res.ok) {
+      setUsers([]);
+      return;
+    }
+
+    const array: user[] = await res.json();
+    setUsers(array);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <Dashboard>
-      <p className="text-xl">Users</p>
+      {fetchStat?.status === 200 && <p className="text-xl">Users</p>}
+      {fetchStat?.status === 200 && (
+        <div className="flex flex-row flex-wrap gap-4 justify-center sm:justify-normal">
+          {users.map((user) => {
+            return (
+              <Card
+                key={user.id}
+                className="flex flex-col p-4 flex-1 min-w-64 max-w-80"
+              >
+                <div className="flex flex-row gap-4 items-center relative">
+                  {user.picture.length ? (
+                    <img
+                      src={user.picture}
+                      alt={`${user.name}'s photo`}
+                      className="rounded-full h-16 w-16"
+                    />
+                  ) : (
+                    <div className="w-16 h-16 bg-slate-500 rounded-full">
+                      <LucideUser className="mx-auto mt-5 scale-[1.5] text-white" />
+                    </div>
+                  )}
+                  <h3 className="font-semibold text-xl text-center">
+                    {user.name}
+                  </h3>
+                  <div className="absolute top-0 right-0 flex flex-row items-center gap-1 text-muted-foreground">
+                    {user.perms.includes("Everything") && <LucideShieldCheck />}
+                    <div
+                      className="block w-5 h-5 rounded-full"
+                      style={{
+                        backgroundColor: `#${user.color}`,
+                      }}
+                    />
+                  </div>
+                </div>
+              </Card>
+            );
+          })}
+        </div>
+      )}
+      {fetchStat !== null && fetchStat.status !== 200 && (
+        <>
+          <h1 className="text-2xl sm:text-4xl text-center">{`${fetchStat.status} - ${fetchStat.statusText}`}</h1>
+        </>
+      )}
     </Dashboard>
   );
 }

f5238fb70a9e9371aee947e74ab7fa81a3b9272d
jakubmanczak <jakub@manczak.net>
2024-11-09 17:42:31 +0100

    de-clientify CardStat

--- facade/src/components/CardStat.tsx
+++ facade/src/components/CardStat.tsx
@@ -1,9 +1,8 @@
-"use client";
-
 import { qfetch } from "@/lib/qfetch";
-import { useEffect, useState } from "react";
 
-const CardStat = (props: {
+const CardStat = async ({
+  variant,
+}: {
   variant:
     | "quoteCount"
     | "weeklyQuoteCount"
@@ -12,73 +11,26 @@ const CardStat = (props: {
     | "authorCount"
     | "quotedAuthorCount";
 }) => {
-  const [stat, setStat] = useState<string>("...");
-  const getQuoteCount = async () => {
-    const res = await qfetch("/quotes/count");
-    if (res.ok) {
-      const text = await res.text();
-      setStat(text);
-    } else setStat("err");
-  };
-  const getWeeklyQuoteCount = async () => {
-    const res = await qfetch("/quotes/count/thisweek");
-    if (res.ok) {
-      const text = await res.text();
-      setStat(text);
-    } else setStat("err");
-  };
-  const getMonthlyQuoteCount = async () => {
-    const res = await qfetch("/quotes/count/thismonth");
-    if (res.ok) {
-      const text = await res.text();
-      setStat(text);
-    } else setStat("err");
-  };
-  const getUserCount = async () => {
-    const res = await qfetch("/users/count");
-    if (res.ok) {
-      const text = await res.text();
-      setStat(text);
-    } else setStat("err");
-  };
-  const getAuthorCount = async () => {
-    const res = await qfetch("/authors/count");
-    if (res.ok) {
-      const text = await res.text();
-      setStat(text);
-    } else setStat("err");
-  };
-  const getQuotedAuthorCount = async () => {
-    const res = await qfetch("/authors/quoted-count");
-    if (res.ok) {
-      const text = await res.text();
-      setStat(text);
-    } else setStat("err");
-  };
-  useEffect(() => {
-    switch (props.variant) {
-      case "userCount":
-        getUserCount();
-        break;
-      case "quoteCount":
-        getQuoteCount();
-        break;
-      case "weeklyQuoteCount":
-        getWeeklyQuoteCount();
-        break;
-      case "monthlyQuoteCount":
-        getMonthlyQuoteCount();
-        break;
-      case "authorCount":
-        getAuthorCount();
-        break;
-      case "quotedAuthorCount":
-        getQuotedAuthorCount();
-        break;
+  const res = await qfetch(
+    variant === "quoteCount"
+      ? "/quotes/count"
+      : variant === "weeklyQuoteCount"
+      ? "/quotes/count/thisweek"
+      : variant === "monthlyQuoteCount"
+      ? "/quotes/count/thismonth"
+      : variant === "userCount"
+      ? "/users/count"
+      : variant === "authorCount"
+      ? "/authors/count"
+      : variant === "quotedAuthorCount"
+      ? "/authors/quoted-count"
+      : "UNREACHABLE",
+    {
+      cache: "no-store",
     }
-  }, []);
-
-  return <>{stat}</>;
+  );
+  const text = await res.text();
+  return text;
 };
 
 export { CardStat };

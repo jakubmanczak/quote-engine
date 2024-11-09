6e6496ce0203ba93ae306e64d31e672922a128d3
jakubmanczak <jakub@manczak.net>
2024-11-10 00:08:50 +0100

    print logs on /logs (ugly)

--- facade/src/app/logs/page.tsx
+++ facade/src/app/logs/page.tsx
@@ -1,15 +1,37 @@
 import { Dashboard } from "@/components/Dashboard";
+import { qfetch } from "@/lib/qfetch";
+import { cookies } from "next/headers";
 
-export default function LogsPage() {
-  // const res = await fetch("localhost:2019/users", {
-  //   cache: "no-cache",
-  //   headers: {},
-  // });
-  // const logs = await res.json();
+export default async function LogsPage() {
+  const cookiestore = cookies();
+  const qauth = cookiestore.get("qauth")?.value;
+  const res = await qfetch("/logs?limit=200&page=1", {
+    cache: "no-cache",
+    headers: {
+      Cookie: `qauth=${qauth}`,
+    },
+  });
+  const logs: {
+    id: string;
+    timestamp: number;
+    actor: string;
+    subject: string;
+    action: { [actionType: string]: unknown };
+  }[] = res.ok && (await res.json());
   return (
     <Dashboard>
       <p className="text-xl">Logs</p>
-      {/* {logs.} */}
+      {res.ok &&
+        logs.map((log) => {
+          const [actionType, actionDetails] = Object.entries(log.action)[0];
+          return (
+            <div key={log.id}>
+              {actionType} <br />
+              {JSON.stringify(actionDetails)}
+            </div>
+          );
+        })}
+      {!res.ok && "Could not fetch logs."}
     </Dashboard>
   );
 }

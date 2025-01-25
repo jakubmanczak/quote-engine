2388ce84bebbc9dc62ccbeb0b046c164d5eeb477
jakubmanczak <jakub@manczak.net>
2025-01-25 21:21:21 +0100

    deal with cors

--- backend/src/router/mod.rs
+++ backend/src/router/mod.rs
@@ -1,6 +1,8 @@
 use crate::state::SharedState;
-use axum::{routing::get, Router};
+use axum::{http::Method, routing::get, Router};
+use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use tower_cookies::CookieManagerLayer;
+use tower_http::cors::CorsLayer;
 
 mod auth;
 mod authors;
@@ -10,6 +12,8 @@ mod quotes;
 mod users;
 
 pub fn init(state: SharedState) -> Router {
+    let allowed_origins = ["http://localhost:3000"];
+
     Router::new()
         .route("/", get(|| async { () }))
         .merge(health::routes())
@@ -20,4 +24,11 @@ pub fn init(state: SharedState) -> Router {
         .merge(quotes::routes())
         .with_state(state)
         .layer(CookieManagerLayer::new())
+        .layer(
+            CorsLayer::new()
+                .allow_origin(allowed_origins.map(|s| s.parse().unwrap()))
+                .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+                .allow_headers([AUTHORIZATION, CONTENT_TYPE])
+                .allow_credentials(true),
+        )
 }
